be2de2324f05daf2da4b06bf2470c5eed9d34760
rtjil <RTJIL@users.noreply.github.com>
2025-07-23 09:23:44 +0000

    src root

--- apps/web/src/modules/pages/home/Home.jsx
+++ apps/web/src/modules/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import styles from './Home.module.css'
 import { Link, useSearchParams } from 'react-router-dom'
-import { buildApiUrl } from 'apps/web/utils/api'
+import { buildApiUrl } from 'apps/web/utils/api.js'
 
 export default function Home({ baseUrl }) {
   const [posts, setPosts] = useState([])
