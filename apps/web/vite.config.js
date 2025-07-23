643b49a3a3eb9da035863afc341c237d864bc80e
rtjil <RTJIL@users.noreply.github.com>
2025-07-23 07:47:43 +0000

    conf

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/* import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
@@ -6,4 +6,19 @@ export default defineConfig({
   server: {
     historyApiFallback: true,
   },
+})
+ */
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+const __dirname = import.meta.dirname
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // confirm it outputs to 'dist' inside apps/web
+  },
+  root: path.resolve(__dirname), // optional but good to be explicit
 })
